dfad625b8c88b19d8c0387fff3f83b8f82035364
Mateo Gravi Fiorino <mateogravi@gmail.com>
2022-08-01 21:23:26 -0300

    Añadimos el segundo script, StatWords

--- statsWords.sh
+++ statsWords.sh
@@ -1,34 +1,47 @@
 #!/bin/bash
 
-#Indicador estadístico de longitud de palabras (la más corta, la más larga y el
-#promedio de longitud).
+#############################################################################################
+#
+# Indicador estadistico de longitud de palabra (mas larga, mas corta y promedio de longitud)
+#
+#############################################################################################
 
+FILE=$1
 
-estadisticasDePalabras(){
-    TEXTO=$1
-    MAS_LARGA=""
-    MAS_CORTA="MESSI"
-    CONTADOR=0
+SOLO_PALABRAS="^[A-Za-z]+$"    #Regex para omitir construcciones de caracteres que no sean palabras.
 
-    for PALABRA in $TEXTO; do
-        if [ ${#PALABRA} -gt ${#MAS_LARGA} ]; then
-            MAS_LARGA=$PALABRA
+MAYOR=0        #Palabra de mayor longitud
+MENOR=0        #Palabra de menor longitud
+CONT=0        #Contador de palabras
+SUMA=0        #Suma total de caracteres del archivo
+
+while read linea; do
+    for palabra in $linea; do
+    #Analizo sólamente las palabras
+        if [[ $palabra =~ $SOLO_PALABRAS ]]; then
+        #Si es la primera palabra leída la guardo como más larga y más corta
+        if [ $CONT == 0 ]; then
+            MAYOR=$palabra
+            MENOR=$palabra
+        else
+        #Si no es la primera palabra la comparo con las palabras guardadas
+            [ ${#palabra} -gt ${#MAYOR} ] && MAYOR=$palabra
+            [ ${#palabra} -lt ${#MENOR} ] && MENOR=$palabra
         fi
-        if [ ${#PALABRA} -lt ${#MAS_CORTA} ] ; then
-            MAS_CORTA=$PALABRA
-        fi
 
-        CONTADOR=$(($CONTADOR + 1))
-
+        ((CONT++))            #Aumento contador
+        SUMA=$((SUMA+${#palabra}))    #Acumulo cantidad de caracteres
+    fi
     done
+done <$FILE
 
-    PROMEDIO=$((${#TEXTO} / $CONTADOR))
+#Calculo promedio de cantidad de caracteres por palabra
+PROMEDIO=$((SUMA/CONT))
 
-    echo "La palabra mas larga del texto es: $MAS_LARGA"
-    echo "La palabra mas corta del texto es: $MAS_CORTA"
-    echo "La cantidad de caracteres por palabra en promedio es: $PROMEDIO"
+echo "Palabra mas larga: $MAYOR - ${#MAYOR} letras"
+#Determino si la palabra mas corta tiene una letra o más para mostrar el mensaje correcto (letra/letras)
+([ ${#MENOR} -eq 1 ] && echo "Palabra mas corta: $MENOR - ${#MENOR} letra") || echo "Palabra mas corta: $MENOR - ${#MENOR} letras" 
+echo "Promedio de longitud de palabras: $PROMEDIO letras"
 
-}
-
-estadisticasDePalabras
+exit 0
 
