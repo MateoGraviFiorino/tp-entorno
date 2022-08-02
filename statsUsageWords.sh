3156f8b51ebabe3c05cf7cb914c65d8516c80192
Mateo Gravi Fiorino <mateogravi@gmail.com>
2022-08-01 21:25:47 -0300

    Añadimos el tercer script, statUsageWords

--- statsUsageWords.sh
+++ statsUsageWords.sh
@@ -1,37 +1,24 @@
 #!/bin/bash
 
-#filtrar por las que tienen al menos 4 letras
+ARCHIVO=$1
 
-contador=0
+INDEX=0
 
-for palabra in $(cat $1 | sort | uniq)
+# grep filtra todas las palabras del archivo 4 o mas de largo.
+# sort las acomoda para podes aplicar uniq -c, que cuenta las apariciones
+# de las lineas (que ahora tiene cada una una palabra) iguales consecutivas
+# luego aplicamos sort con el argumento -r, que revierte el orden del sort
+# #finalmente, aplicamos grep nuevamente para obtener todas las palabras en una lista
+# sin la cantidad de apariciones (que queda como residuo de uniq -c)
+# para la fuente de este codigo, con ciertas modificaciones, visite el archivo README
+for PALABRA in $(cat $ARCHIVO | grep -oE "\w{4,}+" | sort | uniq -c | sort -r)
 do
-	if [ $contador -eq 0 ]
-	then
-	#filtro
-		if [ ${#palabra} -ge 4 ]
-		then
-			for palabra2 in $(cat $1)
-			do
-				if [[ $palabra = $palabra2 ]]
-				then
-					contador=$((contador+1))
-				else
-					continue
-				fi
-			
-				#echo "$contador - $palabra"			
-			done
-			echo "$contador apariciones: $palabra" >> statsUsageWords.txt
-		fi
-	else
-		contador=0
-	fi
+    INDEX=$(($INDEX + 1))
+    if [[ $(($INDEX % 2)) -eq 0 ]]
+    then
+        echo $PALABRA
+    else
+        echo -n "$(($INDEX / 2 + 1)). Con $PALABRA repeticion(es): "
+    fi
+    [[ INDEX -eq 20 ]] && break # Cuando hayamos leido las primer 10 palabras, dejamos de iterar.
 done
-
-
-echo "Top Ten de palabras con mas apariciones: "
-
-cat statsUsageWords.txt | sort | uniq | sort -r | head --lines=10
-
-rm statsUsageWords.txt
