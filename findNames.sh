6d553a93158448f6f84d14ac3f51303c28ac95d4
Mateo Gravi Fiorino <mateogravi@gmail.com>
2022-08-02 23:00:55 -0300

    Actualizacion del script

--- findNames.sh
+++ findNames.sh
@@ -2,9 +2,7 @@
 
 ARCHIVO=$1
 
-# Filtramos las palabras por aquellas que comienzen por una letra mayuscula
-# seguida de una o mas letras minusculas
-for PALABRA in $(cat $ARCHIVO | grep -oE "\w+" | grep -oE "^[A-Z][a-z]+$")
+for PALABRA in $(cat $ARCHIVO | grep -oE "\w+" | grep -oE "^[A-Z][a-z]+$") #grep -oE hace solo los onlymatching y E extended regular expresions
 do
     echo $PALABRA
 done
