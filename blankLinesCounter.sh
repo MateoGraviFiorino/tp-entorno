3ccf8ea6512d2c6d8dc81903137e8e4180255e39
Mateo Gravi Fiorino <mateogravi@gmail.com>
2022-08-02 23:27:56 -0300

    Actualizacion de script

--- blankLinesCounter.sh
+++ blankLinesCounter.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#Se realiza con un filtro grep, el -v selecciona los non-matching lines.
+#Contar lineas en blanco
+#Se realiza con un filtro grep
 RESULTADO=$( grep -v '\S' $1 | grep -v '\a' | wc -l)
 
-echo "La cantidad de lineas en blanco es de: $RESULTADO"
+echo "La cantidad de lineas en blanco en el archivo $1 es de $RESULTADO"
