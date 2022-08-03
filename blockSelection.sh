52b512b219a45f7e66e52607efc6a37b49ff890b
Mateo Gravi Fiorino <mateogravi@gmail.com>
2022-08-02 23:09:06 -0300

    Actualizacion de script

--- blockSelection.sh
+++ blockSelection.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 FILE=$1
-BLOCK=$2
+BLOQUE=$2
 SELECT=$3
 
 #Validación de argumentos
-[[ ! "$BLOCK" =~ [PpOo] ]] && echo "Bloque incorrecto. Ingrese P (párrafo) ׀ O (oración)" && exit 1
+[[ ! "$BLOQUE" =~ ["PpOo"] ]] && echo "Bloque incorrecto. Ingrese P (párrafo) ׀ O (oración)" && exit 1
 [[ ! "$SELECT" =~ [0-9]+ ]] && echo "Número de párrafo/oración inválido" && exit 1
 
 
@@ -13,7 +13,7 @@ FIN_OR="[.!?]+[\s]+?[\n]+?"
 
 
 # Párrafos:
-if [[ "$BLOCK" =~ [Pp] ]]; then
+if [[ "$BLOQUE" =~ [Pp] ]]; then
     # Leo la cantidad de párrafos
     LONG=$(awk -v RS=$FIN_PAR 'END { print NR }' $FILE)
 
@@ -28,7 +28,7 @@ if [[ "$BLOCK" =~ [Pp] ]]; then
 fi
 
 # Oraciones
-if [[ "$BLOCK" =~ [Oo] ]]; then
+if [[ "$BLOQUE" =~ [Oo] ]]; then
            # Leo la cantidad de oraciones. Resto 1 porque considera el EOF como otro record.
     LONG=$(($(awk -v RS=$FIN_OR 'END { print NR }' $FILE)-1))
 
